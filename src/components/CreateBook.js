8be278746cb6b785aacc867653c7f396ab617e72
Kit Jonathan Te <kjonathante@gmail.com>
2018-10-12 22:41:23 -0700

    Add CreateBook component for mutation function

---
+++ src/components/CreateBook.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!) {
+    createBook(description: $description) {
+      _id
+      description
+    }
+  }
+`;
+
+class CreateBook extends Component {
+  state = {
+    description: ""
+  };
+
+  render() {
+    const { description } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
+            type="text"
+            placeholder="A description for the Book"
+          />
+        </div>
+        <Mutation mutation={POST_MUTATION} variables={{ description }}>
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    );
+  }
+}
+
+export default CreateBook;
